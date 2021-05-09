be02b3fc6c9a600eee043fcd3e40bd7ec6b44a54
AnhKhoa1405 <truonganhkhoa1405@gmail.com>
2021-05-09 08:58:25 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      information: {
+        name: "Khoa vip pro",
+        age: 21,
+        job: "Student",
+        company: "QWE",
+      },
+      undone: ["Item1", "Item2", "Item3", "Item4", "Item5"],
+      done: [],
+    };
+  }
+
+  changeName1 = () => {
+    //cách 1
+    // const newInformation = Object.assign(this.state.information, {
+    //   name: "Khoa ngu",
+    // });
+
+    //cách 2 spread operator
+    const newInformation = {
+      ...this.state.information,
+      name: "Khoa ngu",
+    };
+
+    this.setState({
+      information: newInformation,
+    });
+  };
+
+  changeListFromUnToDone = async (index) => {
+    const removeItem = this.state.undone.splice(index, 1);
+    this.state.done.push(removeItem);
+    const newDone = this.state.done;
+    const newUndone = this.state.undone;
+    this.setState({
+      undone: newUndone,
+      done: newDone,
+    });
+  };
+  changeListFromDoneToUn = (index) => {
+    const removeItem = this.state.done.splice(index, 1);
+    this.state.undone.push(removeItem);
+    const newDone = this.state.done;
+    const newUndone = this.state.undone;
+    this.setState({
+      undone: newUndone,
+      done: newDone,
+    });
+  };
+  changeAge = () => {
+    const newInformation = { ...this.state.information, age: 99 };
+    this.setState({
+      information: newInformation,
+    });
+  };
+  changName = () => {
+    // this.setState({
+    //   state1: value,
+    //   state2: value,
+    // });
+    this.setState({
+      information: {
+        name: "Khoa dep trai",
+        age: "12",
+        job: "ABC",
+        company: "HOHO",
+      },
+    });
+  };
+
+  renderUndoneList = () => {
+    return this.state.undone.map((ele, index) => {
+      return (
+        <li
+          id={index}
+          onClick={() => {
+            this.changeListFromUnToDone(index);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {ele}
+        </li>
+      );
+    });
+  };
+  renderDoneList = () => {
+    return this.state.done.map((ele, index) => {
+      return (
+        <li
+          onClick={() => {
+            this.changeListFromDoneToUn(index);
+          }}
+        >
+          {ele}
+        </li>
+      );
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="list">
+            <div className="undoneList">
+              <h1>Todo</h1>
+              <ul>{this.renderUndoneList()}</ul>
+            </div>
+            <div className="doneList">
+              <h1>Complete</h1>
+              <ul>{this.renderDoneList()}</ul>
+            </div>
+          </div>
+
+          {/* <img src={logo} className="App-logo" alt="logo" />
+          <button onClick={this.changName}>Change Information</button>
+          <p onClick={this.changeName1}>Name: {this.state.information.name}</p>
+          <p onClick={this.changeAge}>Age: {this.state.information.age}</p>
+          <p>Job: {this.state.information.job}</p>
+          <p>Company: {this.state.information.company}</p> */}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
